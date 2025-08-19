f11467d883424aa4ef027c8e2bb2532a44e85f92
FloresReaFrankMatias <matiasrea04@gmail.com>
2025-08-19 20:10:29 -0300

    coreccion issue #3

--- tp-1/tp-1.hs
+++ tp-1/tp-1.hs
@@ -229,6 +229,6 @@ sinElPrimero ::[a] -> [a]
 --Precondicion:La lista dada no debe ser vacia
 sinElPrimero (_:xs) = xs
 --6.5
-splitHead ::[a] -> (a,[a])
 --Precondicion:La lista dada no debe ser vacia
-splitHead (x:xs) = (elPrimero(x:xs), sinElPrimero(x:xs) )
+splitHead :: [a] -> (a, [a])
+splitHead (x:xs) = (x, xs)
